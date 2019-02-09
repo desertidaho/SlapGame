8fb4ae9078684f90f9ac650f4a547fbf98f4a3ea
Brett Wilcox <wilcox.brett@gmail.com>
2019-02-08 20:42:07 -0700

    Button logic.

--- game.js
+++ game.js
@@ -1,3 +1,5 @@
+let total = 0;
+
 let target = {
   name: 'happyStick',
   health: 100,
@@ -18,64 +20,121 @@ let items = {
   },
   words: {
     name: 'Words',
-    modifier: 7,
+    modifier: 10,
     description: 'Words will never hurt me. Yay, more power.'
   }
-
 }
 
-function stick() {
-  target.items.push(items.sticks)
+function modifier(type) {
+  if (target.items.length == 0) {
+    target.items.push(items[type])
+    document.getElementById('stick-btn').disabled = true;
+    document.getElementById('stone-btn').disabled = true;
+  } else {
 
-}
-
-function stone() {
-  target.items.push(items.stones)
+  }
 }
 
 function word() {
-  target.items.push(items.words)
+  if (target.health <= 90) {
+    target.health += 10;
+  }
+  update()
 }
-
-let total = 0;
 
 function addMods() {
   for (let i = 0; i < target.items.length; i++) {
     total += target.items[i].modifier
   }
-
   return total;
 }
 
-
-function slap() {
+function attack(type) {
   addMods()
-  target.health -= total;
-  target.health--;
-  target.hits++;
-
+  if (target.health >= total + 1) {
+    target.health -= total;
+    target.hits++;
+    if (type == 'slap') {
+      target.health--;
+    } else if (type == 'punch') {
+      target.health -= 5;
+    } else {
+      if (target.health >= 10) {
+        target.health -= 10;
+      }
+    }
+  }
   update()
-}
-
-function punch() {
-  target.health -= 5;
-  target.hits++;
-
-  update()
-  addMods()
-}
-
-function kick() {
-  target.health -= 10;
-  target.hits++;
-
-  update()
-  addMods()
 }
 
 function update() {
   document.getElementById('health').innerText = target.health.toString();
   document.getElementById('hits').innerText = target.hits.toString();
 
+  if (target.health >= 1) {
+    document.getElementById('slap1').disabled = false;
+  } else {
+    document.getElementById('slap1').disabled = true;
+  }
+  if (target.health >= 5) {
+    document.getElementById('punch5').disabled = false;
+  } else {
+    document.getElementById('punch5').disabled = true;
+  }
+  if (target.health >= 10) {
+    document.getElementById('kick10').disabled = false;
+  } else {
+    document.getElementById('kick10').disabled = true;
+  }
+
+  document.getElementById('stick-btn').disabled = false;
+  document.getElementById('stone-btn').disabled = false;
+  if (target.health <= 90) {
+    document.getElementById('word-btn').disabled = false;
+  } else {
+    document.getElementById('word-btn').disabled = true;
+  }
+
   total = 0;
+  target.items = [];
 }
+
+
+/*
+old code, refactored above
+
+function slap() {
+  addMods()
+  if (target.health >= total + 1) {
+    target.health -= total;
+    target.health--;
+    target.hits++;
+  }
+  update()
+}
+
+function punch() {
+  addMods()
+  if (target.health >= total + 1) {
+    target.health -= total;
+    target.health -= 5;
+    target.hits++;
+  }
+  update()
+}
+
+function kick() {
+  addMods()
+  if (target.health >= total + 1) {
+    target.health -= total;
+    target.health -= 10;
+    target.hits++;
+  }
+  update()
+}
+
+function stone() {
+  target.items.push(items.stones)
+}
+
+*/
